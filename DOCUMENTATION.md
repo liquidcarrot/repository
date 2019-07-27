d4e42f34fcef3c8dfd4b15be98c1ad723f6ffd3a
luiscarbonell <luiscarbonell5683@gmail.com>
2019-07-26 20:46:22 -0400

    Added Documentation, Documentation Scripts, & Config Files

---
+++ DOCUMENTATION.md
@@ -0,0 +1,18 @@
+<a name="sum"></a>
+
+## sum([...numbers]) ⇒ <code>number</code>
+**Kind**: global function  
+**Returns**: <code>number</code> - Returns the sum of the numbers passed  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| [...numbers] | <code>Array.&lt;number&gt;</code> | A series or array of numbers |
+
+**Example**  
+```js
+sum(1, 3, 4) // 8
+
+sum([3, 6]) // 9
+
+sum() // 0
+```
--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 /**
+ * @param {number[]} [numbers] A series or array of numbers
+ *
+ * @returns {number} Returns the sum of the numbers passed
+ *
  * @example
  * sum(1, 3, 4) // 8
  *
- * @example
  * sum([3, 6]) // 9
  *
- * @example
  * sum() // 0
- *
- * @param {number[]} arguments An array of numbers
- *
- * @returns {number} Returns the sum of the numbers passed
  */
-function sum(...arguments) {
+function sum(...numbers) {
   // For `sum([numbers])`
-  if(arguments.length === 1 && Array.isArray(arguments[0])) arguments = arguments[0];
+  if(numbers.length === 1 && Array.isArray(numbers[0])) numbers = numbers[0];
   
   // Sum of `arguments`
-  return arguments.reduce(function(sum, value) {
+  return numbers.reduce(function(sum, value) {
     return sum += value;
   }, 0)
 }
